97f66c2035566f5ac8489b4f9613df881536f52a
Gayathri Avuthu <avuthugayathri@gamil.com>
2024-12-07 12:11:49 +0530

    jfsdlab

---
+++ src/main/java/com/klef/jfsd/exam/ClientDemo.java
@@ -0,0 +1,18 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ClientDemo {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.java);
+
+        // Demonstrating Constructor Injection
+        Employee employee = context.getBean(Employee.class);
+        System.out.println(employee);
+
+        // Demonstrating Setter Autowiring
+        Course course = context.getBean(Course.class);
+        System.out.println(course);
+    }
+}
